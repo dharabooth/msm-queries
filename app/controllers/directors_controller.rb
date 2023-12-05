7c677c60401bd10e71d9455faa912bcf611ebae3
dharabooth <146215658+dharabooth@users.noreply.github.com>
2023-12-05 05:55:11 +0000

    everything except junior and senior

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,35 @@
+class DirectorsController < ApplicationController
+
+  def index
+    render({ :template => "directors_templates/list"})
+  end
+
+  def show
+    the_id = params.fetch("the_id")
+
+    matching_records = Director.where({ :id => the_id})
+
+    @the_director = matching_records.at(0)
+
+    render({ :template => "directors_templates/details"})
+
+  end
+
+  def junior
+    non_nulls = Director.all.where.not({ :dob => nil})
+    organized_records = non_nulls.order({ :dob => :desc})
+    @junior_director = organized_records.at(0)
+
+    render({ :template => "directors_templates/junior"})
+  end
+
+  def senior
+    non_nulls = Director.all.where.not({ :dob => nil})
+    organized_records = non_nulls.order({ :dob => :asc})
+    @senior_director = organized_records.at(0)
+
+    render({ :template => "directors_templates/senior"})
+  end
+
+
+end
